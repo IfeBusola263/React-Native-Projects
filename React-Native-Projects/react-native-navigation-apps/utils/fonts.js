685445f5cbdde4098e111f46bab1f43513025c97
Busola <ifebusolaj95@gmail.com>
2024-09-11 03:40:06 +0100

    Changed the font for the HeaderLargeTitle

--- React-Native-Projects/react-native-navigation-apps/utils/fonts.js
+++ React-Native-Projects/react-native-navigation-apps/utils/fonts.js
@@ -1,8 +1,10 @@
 export const fonts = {
-    'open-sans-bold' : require('../assets/fonts/OpenSans-Bold.ttf'),
-    'open-sans' : require('../assets/fonts/OpenSans-Regular.ttf'),
-    'montserrat-bold': require('../assets/fonts/MontserratAlternates-Bold.otf'),
-    'exo-semi-bold': require('../assets/fonts/Exo-SemiBold.otf'),
-    'poppins-light': require('../assets/fonts/Poppins-Light.otf'),
-    'muli-bold': require('../assets/fonts/Muli-Bold.ttf')
-}
+  "open-sans-bold": require("../assets/fonts/OpenSans-Bold.ttf"),
+  "open-sans": require("../assets/fonts/OpenSans-Regular.ttf"),
+  "montserrat-bold": require("../assets/fonts/MontserratAlternates-Bold.otf"),
+  "exo-semi-bold": require("../assets/fonts/Exo-SemiBold.otf"),
+  "poppins-light": require("../assets/fonts/Poppins-Light.otf"),
+  "muli-bold": require("../assets/fonts/Muli-Bold.ttf"),
+  "cooper-hewitt-bold": require("../assets/fonts/CooperHewitt-Bold.otf"),
+  bebas: require("../assets/fonts/Bebas-Regular.ttf"),
+};
--- React-Native-Projects/react-native-navigation-apps/utils/screenOptions.js
+++ React-Native-Projects/react-native-navigation-apps/utils/screenOptions.js
@@ -1,44 +1,49 @@
-import {Platform} from 'react-native';
-import HeaderRight from '../components/HeaderRight.jsx';
+import { Platform } from "react-native";
+import HeaderRight from "../components/HeaderRight.jsx";
 
 export const ScreenOptions = {
-    autoHideHomeIndicator: true,
-    animationTypeForReplace: 'push',
-    // animation: 'slide_from_bottom',
-    // presentation: 'modal',
-    headerBackButtonMenuEnabled: false,
-    headerStyle: {backgroundColor: '#351401'},
-    headerTintColor: '#fafafd',
-    headerBackVisible: true,
-    headerBackTitle: 'Go Back', // only on IOS
-    headerBackTitleVisible: true, // makes the title on the back button disappear, only on IOS
-    headerBackTitleStyle: {fontSize: 14}, // only IOS
-    headerLargeTitle: true, // IOS only, has an amazing effect on header, but you need a scrollView or flatList and set the contentInsetAdjustmentBehavior="automatic" on the component
-    headerLargeTitleShadowVisible: true,
-    headerLargeTitleStyle: {fontSize: 32, fontWeight: 'bold', color: 'rgba(250, 250, 250, 0.75)'}, // the style of the large header before it collapses after a scroll
-    headerShown: true, // trigger if the entire header is shown or not, not just the title
-    headerShadowVisible: true,
-    headerTransparent: Platform.select({ios: true, android: false}),
-    headerBlurEffect: 'dark',
-    headerTitleAlign: 'center',
-    headerSearchBarOptions: {
-	autoCapitalize: 'words',
-	autoFocus: false,
-	placeholder: 'Search', // only on Android
-	disableBackButtonOverride: true, // only on android
-	hintTextColor: '#fafdfb'
-    },
-    contentStyle: {
-	backgroundColor: '#24180f',
-    }
-    // statusBarAnimation: 'slide'
-}
-
+  autoHideHomeIndicator: true,
+  animationTypeForReplace: "push",
+  // animation: 'slide_from_bottom',
+  // presentation: 'modal',
+  headerBackButtonMenuEnabled: false,
+  headerStyle: { backgroundColor: "#351401" },
+  headerTintColor: "#fafafd",
+  headerBackVisible: true,
+  headerBackTitle: "Go Back", // only on IOS
+  headerBackTitleVisible: true, // makes the title on the back button disappear, only on IOS
+  headerBackTitleStyle: { fontSize: 14 }, // only IOS
+  headerLargeTitle: true, // IOS only, has an amazing effect on header, but you need a scrollView or flatList and set the contentInsetAdjustmentBehavior="automatic" on the component
+  headerLargeTitleShadowVisible: true,
+  headerLargeTitleStyle: {
+    fontSize: 48,
+    fontFamily: "bebas",
+    fontWeight: "bold",
+    color: "rgba(250, 250, 250, 0.75)",
+  }, // the style of the large header before it collapses after a scroll
+  headerShown: true, // trigger if the entire header is shown or not, not just the title
+  headerShadowVisible: true,
+  headerTransparent: Platform.select({ ios: true, android: false }),
+  headerBlurEffect: "dark",
+  headerTitleAlign: "center",
+  headerSearchBarOptions: {
+    autoCapitalize: "words",
+    autoFocus: false,
+    placeholder: "Search", // only on Android
+    disableBackButtonOverride: true, // only on android
+    hintTextColor: "#fafdfb",
+  },
+  contentStyle: {
+    backgroundColor: "#24180f",
+  },
+  headerTitleStyle: { fontFamily: "cooper-hewitt-bold" },
+  // statusBarAnimation: 'slide'
+};
 
 export const MealScreenOptions = {
-    headerRight: () => <HeaderRight />
-}
+  headerRight: () => <HeaderRight />,
+};
 
 export const CategoriesScreenOptions = {
-    title: 'All Categories'
-}
+  title: "All Categories",
+};
